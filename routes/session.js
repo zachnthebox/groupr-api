243a915ff137527f961ac5740e8d3d2f19e76dc2
Zachary Berardo <zachary@zachnthebox.com>
2018-03-04 15:05:25 -0500

    feat(auth): tokens can variable expiration

--- routes/session.js
+++ routes/session.js
@@ -8,8 +8,8 @@ const router = express.Router();
 const firebase_api_key = process.env.firebase_api_key;
 
 const makeRequest = options => {
-  return new RSVP.Promise(function(resolve, reject) {
-    request(options, function(error, response, body) {
+  return new RSVP.Promise(function (resolve, reject) {
+    request(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         resolve(body);
       }
@@ -18,10 +18,20 @@ const makeRequest = options => {
   });
 };
 
-router.post('/', function(req, res) {
+const generateToken = (data, expiresIn) => {
+  const token = JWT.sign({
+    data,
+  }, process.env.jwt_secret, {
+      expiresIn,
+    });
+  return token;
+};
+
+router.post('/', function (req, res) {
   const session = req.body.session;
   const email = session.email;
   const password = session.password;
+  const expiresIn = session.expiresIn || '1h';
   makeRequest({
     uri: `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${firebase_api_key}`,
     method: 'POST',
@@ -30,13 +40,10 @@ router.post('/', function(req, res) {
       password,
     },
   }).then(response => {
-    const token = JWT.sign({
-      data: {
-        email,
-      },
-    }, 'shhhhhhared-secret', {
-      expiresIn: '1h'
-    });
+    const token = generateToken({
+      email,
+    }, expiresIn);
+
     res.json({
       session: {
         email,
